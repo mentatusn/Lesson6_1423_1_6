08da02c616214cffd219871d8f8f85ac9c03e323
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-03 23:34:18 +0300

    Исправили ошибку с onBackPressed(). Пришлоись перенести ее в onResume()
    Дело в том, что эмулировать  нажатие onBackPressed в методе onCreate() - та еще лотерея. Ведь активити находится еще в зачаточном состоянии. А вот в onResume() у нас уже есть полный контроль и управление.

--- app/src/main/java/calculator/calulation/lesson6_1423_1_6/CitiesFragment.java
+++ app/src/main/java/calculator/calulation/lesson6_1423_1_6/CitiesFragment.java
@@ -1,9 +1,14 @@
 package calculator.calulation.lesson6_1423_1_6;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,15 +16,89 @@ import androidx.fragment.app.Fragment;
 
 public class CitiesFragment extends Fragment {
 
-
+     City currentCity;
+     public static String KEY_CITY = "city";
+     boolean isLandScape;
 
     public static CitiesFragment newInstance(){
         return new CitiesFragment();
     }
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        isLandScape = getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE;
+
+        Log.d("mylogs","savedInstanceState!=null "+(savedInstanceState!=null));
+        if(savedInstanceState!=null){
+            currentCity = savedInstanceState.getParcelable(KEY_CITY);
+        }
+        Log.d("mylogs","currentCity!=null "+(currentCity!=null));
+        if(isLandScape)
+            if(currentCity!=null){
+                showCoatOfArms(currentCity.getImageIndex());
+            }else{
+                showCoatOfArms(0);
+            }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(KEY_CITY,currentCity);
+        super.onSaveInstanceState(outState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cities,container,false);
+        LinearLayout linearLayout = (LinearLayout) view;
+
+        String[] cities = getResources().getStringArray(R.array.cities);
+
+        for(int i =0;i<cities.length;i++){
+            String name = cities[i];
+            TextView textView = new TextView(getContext());
+            textView.setText(name);
+            textView.setTextSize(30);
+            linearLayout.addView(textView);
+            int finalI = i;
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                      showCoatOfArms(finalI);
+                }
+            });
+        }
+
         return view;
+    }
+
+    private void showCoatOfArms(int index) {
+        currentCity = new City(index,
+                (getResources().getStringArray(R.array.cities)[index]));
+        if (isLandScape) {
+            showCoatOfArmsLand();
+        }else{ // port
+            showCoatOfArmsPort();
+        }
+    }
+
+    private void showCoatOfArmsPort() {
+        requireActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.cities_container, CoatOfArmsFragment.newInstance(currentCity))
+                .addToBackStack("")
+                .commit();
+    }
+
+    private void showCoatOfArmsLand() {
+        requireActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.coat_of_arms_container, CoatOfArmsFragment.newInstance(currentCity))
+                .commit();
     }
 }
--- app/src/main/java/calculator/calulation/lesson6_1423_1_6/MainActivity.java
+++ app/src/main/java/calculator/calulation/lesson6_1423_1_6/MainActivity.java
@@ -1,6 +1,7 @@
 package calculator.calulation.lesson6_1423_1_6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -11,17 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.cities,CitiesFragment.newInstance())
-                .commit();
-        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+        if(savedInstanceState==null){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.coat_of_arms,CoatOfArmsFragment.newInstance(new City(0,"test")))
+                    .replace(R.id.cities_container,CitiesFragment.newInstance())
                     .commit();
         }
 
+    }
+
+
+    /** Пришлось перенести наш костыль в onResume
+     * так как не onBackPressed() вызывать в onCreate - черевато
+     **/
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ищем фрагмент, который сидит в контейнере R.id.cities_container
+        Fragment backStackFragment = (Fragment)getSupportFragmentManager()
+                .findFragmentById(R.id.cities_container);
+        // если такой есть, и он является CoatOfArmsFragment
+        if(backStackFragment!=null&&backStackFragment instanceof CoatOfArmsFragment){
+            //то сэмулируем нажатие кнопки Назад
+            onBackPressed();
+        }
     }
 }
