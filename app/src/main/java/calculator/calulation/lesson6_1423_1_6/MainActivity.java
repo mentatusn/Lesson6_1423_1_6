7d842b3ff844dccb16bba99a537972c163c705f7
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-03 20:21:57 +0300

    Initial commit

---
+++ app/src/main/java/calculator/calulation/lesson6_1423_1_6/MainActivity.java
@@ -0,0 +1,14 @@
+package calculator.calulation.lesson6_1423_1_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
